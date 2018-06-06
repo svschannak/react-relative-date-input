e18eb6ea17b2bbdbf6820a43177716905b5a24f0
Sven Schannak <svschannak@googlemail.com>
2018-06-06 08:05:35 +0200

    Make package working

---
+++ index.js
@@ -0,0 +1,121 @@
+import React from "React";
+import PropTypes from 'prop-types';
+import moment from "moment";
+
+const propTypes = {
+    onChange: PropTypes.func.isRequired,
+    numberInputClassName: PropTypes.string,
+    numberInputStyle: PropTypes.object,
+    selectClassName: PropTypes.string,
+    selectStyle: PropTypes.object,
+    wrapperClassName: PropTypes.string,
+    wrapperStyle: PropTypes.object,
+    labelClassName: PropTypes.string,
+    labelStyle: PropTypes.object,
+    labelText: PropTypes.string,
+    monthsLabel: PropTypes.string,
+    daysLabel: PropTypes.string,
+    weeksLabel: PropTypes.string,
+    yearsLabel: PropTypes.string,
+}
+
+const defaultProps = {
+    onChange: () => {},
+    numberInputClassName: "",
+    numberInputStyle: {},
+    selectClassName: "",
+    selectStyle: {},
+    wrapperClassName: "",
+    wrapperStyle: {},
+    labelClassName: "",
+    labelStyle: {},
+    labelText: "Relatives Datum",
+    monthsLabel: "Monate",
+    daysLabel: "Tage",
+    weeksLabel: "Wochen",
+    yearsLabel: "Jahre"
+}
+
+class RelativeDatePicker extends React.Component {
+    state = {
+        number: null,
+        periodType: "months",
+        chosenDate: null
+    };
+
+    changePeriodType = e => {
+        this.setState(
+            {
+                periodType: e.target.value
+            },
+            () => {
+                this.transformData();
+            }
+        );
+    };
+
+    changeNumber = e => {
+        this.setState(
+            {
+                number: e.target.value
+            },
+            () => {
+                this.transformData();
+            }
+        );
+    };
+
+    transformData = () => {
+        const newChosenData = moment().add(
+            this.state.number,
+            this.state.periodType
+        );
+        this.setState(
+            {
+                chosenDate: newChosenData
+            },
+            () => {
+                this.props.onChange(this.state.chosenDate);
+            }
+        );
+    };
+
+    onChangeTrigger = () => {
+        this.transformData();
+    };
+
+    render() {
+        return (
+            <div
+                class={this.props.wrapperClassName}
+                style={this.props.wrapperStyle}
+            >
+                <span style={this.props.labelStyle}>{this.props.labelText}</span>
+                <input
+                    type="number"
+                    value={this.state.number}
+                    onChange={this.changeNumber}
+                    style={this.props.numberInputStyle}
+                    className={this.props.numberInputClassName}
+                />
+                <select
+                    onChange={this.changePeriodType}
+                    value={this.state.periodType}
+                    style={this.props.selectStyle}
+                    className={this.props.selectClassName}
+                >
+                    <option value="months">{this.props.monthsLabel}</option>
+                    <option value="days">{this.props.daysLabel}</option>
+                    <option value="weeks">{this.props.weeksLabel}</option>
+                    <option value="years">{this.props.yearsLabel}</option>
+                </select>
+            </div>
+        );
+    }
+}
+
+
+RelativeDatePicker.propTypes = propTypes;
+RelativeDatePicker.defaultProps = defaultProps;
+
+export default RelativeDatePicker;
---
+++ readme.md
@@ -0,0 +1,52 @@
+# React relative date input
+
+Input and select for choosing an relative date in the future.
+
+## Example
+![Example for react relative date input](example.png "Example for react relative date input")
+
+## Installation
+
+```bash
+yarn add react-relative-date-input
+```
+
+## Usage
+```js
+import RelativeDatePicker from "react-relative-date-input";
+
+const onChange = (momentDate) => {
+    // gives you a moment.js date
+    console.log(momentDate);
+}
+
+const YourForm = () => {
+    return (
+        <RelativeDatePicker
+            onChange={onChange}
+        />
+    )
+}
+
+```
+
+## Props
+
+|Prop name|Description|Example values|
+|----|----|----|
+|onChange|Function that is called if the number or period type is changed|(momentDate) => {console.log(momentDate)};|
+|numberInputClassName|CSS classes for number input field|"form-control"|
+|numberInputStyle|Object with styles for number input field|{ fontSize: "14px" }|
+|wrapperClassName|CSS Classes for number input field|"col-md-12"|
+|wrapperStyle|Object with styles for wrapper|{ padding: "5px" }|
+|labelClassName|CSS classes for label|"label-class"|
+|labelStyle|Object with styles for label|{ fontSize: "14px" }|
+|selectClassName|CSS classes period type select field|"form-control"|
+|selectStyle|Object with styles for period type select field|{ fontSize: "14px" }|
+|labelText|Text for label|"Relative date"|
+|monthsLabel|Text for months in select|"Months"|
+|daysLabel|Text for days in select|"Days"|
+|weeksLabel|Text for weeks in select|"Weeks"|
+|yearsLabel|Text for years in select|"Years"|
+
+
